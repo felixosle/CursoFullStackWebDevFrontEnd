70f9ac2d0be595e9974db38ea2eb5dd3a5e7edee
Felix Martinez <maroslfe@jcyl.es>
2018-04-09 16:08:03 +0200

    Instalado Angular Flex-layout

--- proyecto-angular/src/app/app.module.ts
+++ proyecto-angular/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MatButtonModule, MatIconModule} from '@angular/material'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule } from '@angular/material'
 
 
 import { AppComponent } from './app.component';
@@ -9,6 +9,7 @@ import { ReservasComponent } from './reservas/reservas.component';
 import { EdificiosComponent } from './edificios/edificios.component';
 import { SalasComponent } from './edificios/salas/salas.component';
 import { AppRoutingModule } from './app-routing.module';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 
 @NgModule({
@@ -22,8 +23,8 @@ import { AppRoutingModule } from './app-routing.module';
     BrowserModule,
     BrowserAnimationsModule,
     AppRoutingModule,
-    MatButtonModule,
-    MatIconModule
+    MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]

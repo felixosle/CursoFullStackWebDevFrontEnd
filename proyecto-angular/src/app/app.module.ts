f0b5aa45fb57a7981ae19428499e15a01d3ae77e
Felix Martinez <maroslfe@jcyl.es>
2018-06-08 11:12:07 +0200

    Correción

--- proyecto-angular/src/app/app.module.ts
+++ proyecto-angular/src/app/app.module.ts
@@ -3,8 +3,7 @@ import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 // Práctica 4.3: Importar API Swagger
-// Alguien debería importar el módulo de Swagger (api.module) y el servicio default.service:
-
+// Alguien debería importar el módulo de Swagger (api.module):
 
 import { EdificioMockService } from './edificios/edificio.mock.service';
 import { ReservaMockService } from './reservas/reserva.mock.service';
@@ -69,14 +68,12 @@ import { NuevaSalaComponent } from './edificios/salas/nueva-sala/nueva-sala.comp
     MatMomentDateModule, MatSidenavModule, MatToolbarModule, MatListModule, MatTabsModule, MatSnackBarModule,
     MatCardModule, MatSelectModule, MatProgressSpinnerModule, MatDialogModule, FlexLayoutModule,
     MatTableModule, MatSortModule, MatPaginatorModule, MatTooltipModule, MatMenuModule, MatAutocompleteModule,
-    FormsModule, ReactiveFormsModule, HttpClientModule, 
-    // Práctica 4.3: Importar API Swagger
-    // Alguien debería incluir el módulo de Swagger (api.module) en imports:
+    FormsModule, ReactiveFormsModule, HttpClientModule
     
   ],
   providers: [
     // Práctica 4.3: Importar API Swagger
-    // Alguien debería declarar el servicio default.service como proveedor:
+    // Alguien debería declarar  el módulo de Swagger como proveedor:
     
     SalasMockService,
     EdificioMockService,
